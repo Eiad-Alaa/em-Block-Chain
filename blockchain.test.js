285b1ed1dc85a5428405ed3e5b03fbc218768220
Eiad-Alaa <eiad.alaa.nouh@gmail.com>
2025-04-23 21:40:03 +0200

    Add chain validation and test validation

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,11 @@ const Blockchain = require("./blockchain");
 const Block = require("./block");
 
 describe('Blockchain', () => {
-  let blch;
+  let blch, blch2;
 
   beforeEach(() => { 
     blch = new Blockchain();
+    blch2 = new Blockchain();
   }
   );
 
@@ -17,6 +18,26 @@ describe('Blockchain', () => {
     const data = 'eiad';
     blch.addBlock(data);
     expect(blch.chain[blch.chain.length - 1].data).toEqual(data);
+  }
+  );
+
+  it('validates a valid chain', () => {
+    blch2.addBlock('eiad');
+    blch2.addBlock('eiad2');
+    expect(blch.isValidChain(blch2.chain)).toBe(true);
+  }
+  );
+
+  it('validates a chain with a corrupt genesis block', () => {
+    blch2.chain[0].data = 'zongo data';
+    expect(blch.isValidChain(blch2.chain)).toBe(false);
+  }
+  );
+
+  it('invalidates a corrupt chain', () => {
+    blch2.addBlock('eiad');
+    blch2.chain[1].data = 'zongo data';
+    expect(blch.isValidChain(blch2.chain)).toBe(false);
   }
   );
 }
